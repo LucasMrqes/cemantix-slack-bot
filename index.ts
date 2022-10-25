553537bbaa5eb493475d76a020e3f5ece7d60960
LucasMrqes <lucas.marques@student-cs.fr>
2022-10-25 15:57:10 +0200

    Added faunadb

--- index.ts
+++ index.ts
@@ -1,12 +1,88 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
+import {
+    json,
+    validateRequest,
+  } from "https://deno.land/x/sift@0.6.0/mod.ts";
+  
+
+const wordId = 346498445402440265;
+updateWordToFind();
+
+  async function queryFauna(
+    query: string,
+  ) {
+    // Grab the secret from the environment.
+    const token = Deno.env.get("FAUNA_SECRET");
+    if (!token) {
+      throw new Error("environment variable FAUNA_SECRET not set");
+    }
+  
+    try {
+      // Make a POST request to fauna's graphql endpoint with body being
+      // the query and its variables.
+      const res = await fetch("https://graphql.fauna.com/graphql", {
+        method: "POST",
+        headers: {
+          authorization: `Bearer ${token}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          query,
+        }),
+      });
+  
+      const { data, errors } = await res.json();
+      if (errors) {
+        // Return the first error if there are any.
+        return { data, error: errors[0] };
+      }
+  
+      return { data };
+    } catch (error) {
+      return { error };
+    }
+  }
+
+async function updateWordToFind() {
+    const text = await Deno.readTextFile('liste_francais.txt');
+    const words = text.split('\n');
+    const updatedWord = words[Math.floor(Math.random()*words.length)];
+    const query = `
+    mutation {
+        updateWord(id: 346498445402440265, data: {
+            word: ${updatedWord}
+        }) {
+            _id
+            ${wordId}
+        }
+    }
+    `;
+    const {data,error} = await queryFauna(query);
+    if (error) {
+        return error;
+    }
+    return data;
+}
+
+async function getWordToFind() {
+    const query = `
+    query {
+        findWordByID(id: ${wordId}) {
+            word
+        }
+    }
+    `;
+    const {data,error} = await queryFauna(query);
+    if (error) {
+        return error;
+    }
+    return data;
+}
 
 
 async function handler(_req: Request): Promise<Response> {
     try {
-        //const text = await Deno.readTextFile('liste_francais.txt');
-        //const words = text.split('\n');
-        //const wordToFind = words[Math.floor(Math.random()*words.length)];
-        const wordToFind = "Chien";
+        const wordToFind = getWordToFind().data.findWordByID.word;
         const guess = await extractGuess(_req);
         console.log(`Guess detecté ${guess}.`);
         console.log(`Mot a trouver ${wordToFind}.`);
@@ -43,6 +119,7 @@ const similarity = async (word1, word2) => {
 
 const responseBuilder = async (word: string, similarity: number) => {
     if (similarity == 1) {
+        updateWordToFind();
         return `Trouvé ! Le mot était ${word}. \n Le mot a changé, rejoue !`;
       } else if (similarity > 0.5) {
         return `Tu chauffes, ${word} est proche du mot à trouver, score : ${similarity}`;
